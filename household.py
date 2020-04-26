cbb6df711abd369dc67c2bfc70446e06c4f72c43
Nate Strawser <ns4ek@virginia.edu>
2020-04-25 21:51:01 -0400

    sample animation

--- household.py
+++ household.py
@@ -22,3 +22,44 @@ def get_coordinates():
 
 
 print(get_coordinates())
+
+
+
+# =================================================================
+
+import matplotlib.pyplot as plt 
+from matplotlib.animation import FuncAnimation
+import numpy as np
+
+dt = 0.005
+n=20
+L = 1
+houses=np.zeros(n,dtype=[("position", float , 2),
+                            ("velocity", float ,2),
+                            ("force", float ,2),
+                            ("size", float , 1)])
+
+houses["position"]=np.random.uniform(0,L,(n,2))
+houses["velocity"]=np.zeros((n,2))
+houses["size"]=0.5*np.ones(n)
+
+fig = plt.figure(figsize=(7,7))
+ax = plt.axes(xlim=(0,L),ylim=(0,L))
+
+print(houses["position"][:,0])
+x = [0.40288222,0.17266386]
+y = [0.5, 0.2]
+scatter=ax.scatter(x, y)
+
+def update(frame_number):
+    houses["force"]=np.random.uniform(-2,2.,(n,2))
+    houses["velocity"] = houses["velocity"] + houses["force"]*dt
+    houses["position"] = houses["position"] + houses["velocity"]*dt
+
+    houses["position"] = houses["position"]%L
+    scatter.set_offsets(houses["position"])
+    return scatter, 
+    # return
+
+anim = FuncAnimation(fig, update, interval=10)
+plt.show() 
